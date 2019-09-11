83d612cbe5e4525b28a86ded357def1e7f0f88d4
troydaniels <troyaadaniels@gmail.com>
2019-09-12 00:26:55 +1000

    Refactor appointmentController to filter by date range

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,8 +1,19 @@
 const Appointment = require('../models/appointment');
+const moment = require('moment');
 
-// GET Appointment list for a given practitioner_id
+// Retrieve Appointment list for a given practitioner_id, between start_date and end_date
 exports.index = async (req, res) => {
-    const { practitioner_id } = req.body;
-    const appointments = await Appointment.find({practitioner_id}).exec();
-    res.send(appointments);
+    const { practitioner_id, start_date = new Date(0), end_date = new Date() } = req.body;
+    // Mongoose doesn't like the $gte, $lte date filtering, likely because of it's format in the original JSON
+    const appointments = await Appointment.find({practitioner_id: practitioner_id}).exec();
+
+    const filteredAppointments = [];
+    appointments.forEach(appointment => {
+        const { date } = appointment;
+        if(date >= start_date && date <= end_date) {
+            filteredAppointments.push(appointment);
+        }
+    });
+
+    res.send(filteredAppointments);
 };
--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Require controller modules.
 const appointment_controller = require('../controllers/appointmentController');
 
-// GET Appointment list for given practitioner_id
+// Retrieve Appointment list for a given practitioner_id, between start_date and end_date
 router.post('/', appointment_controller.index);
 
 module.exports = router;
